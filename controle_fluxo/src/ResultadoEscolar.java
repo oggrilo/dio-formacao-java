ea42e6313d66538ae3ddaa8c6b085057b509424d
Murilo <103610241+oggrilo@users.noreply.github.com>
2025-04-29 15:34:58 -0300

    aprimoramento no formato de verificação de nota

--- controle_fluxo/src/ResultadoEscolar.java
+++ controle_fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,12 @@
 // ResultadoEscolar.java
+// Cenário 1
+// Cenário 2
 public class ResultadoEscolar {
-    public static void main(String[] args) {
-
-       int nota = 7;
-       
-       if(nota >= 7)
-            System.out.println("Aluno(a) Aprovado");
-        else if (nota >=5 && nota<7)
-            System.out.println("Aluno(a) em recuperação");
-       else
-        System.out.println("Aluno(a) Reprovado");
-    }
+	public static void main(String[] args) {
+		int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+	}
 }
+
+	
